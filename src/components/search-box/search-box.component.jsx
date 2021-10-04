7e601fe513c0196c37f1cc37f4461afce36e1a21
Trever Pehrson <tpehrson@staywell.com>
2021-10-04 15:41:36 -0600

    adding pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './search-box.styles.css'
+
+export const SearchBox = ({ placeholder, handleOnChange }) => (
+  <input
+    className="search"
+    type="search" 
+    placeholder= {placeholder}
+    onChange= {handleOnChange}
+  />
+)
